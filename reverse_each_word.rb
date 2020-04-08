3a615440c7b654e78a9a92feb571862b1a616db2
Caroline Glass <carolineglass89@gmail.com>
2020-04-08 18:16:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   split_sentence = sentence.split
   word_reverse = []
   split_sentence.each do |word| 
-    word.reverse
+    word_reverse.push(word.reverse)
   end 
-  
+  word_reverse
 end 
